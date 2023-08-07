32c1cce85d7445555baa689d47115e18edb69ce9
nderituc <nderituc@mail.gvsu.edu>
2023-08-07 07:31:36 -0400

    Update

---
+++ webAPP/app.R
@@ -0,0 +1,52 @@
+# Load required libraries
+library(shiny)
+library(shinydashboard)
+
+# UI
+ui <- dashboardPage(
+  dashboardHeader(title = "ACN"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Introduction", tabName = "intro_tab"),
+      menuItem("Data Insights", tabName = "data_tab"),
+      menuItem("PDF Files", tabName = "pdf_tab")
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      # Introduction tab
+      tabItem(
+        tabName = "intro_tab",
+        h2("Introduction to African Collaborative Network"),
+        p("Welcome to ACN! This is the introduction tab where you can find information about ACN and other details."),
+        # Add any other UI elements for introduction here
+      ),
+      # Data Insights tab
+      tabItem(
+        tabName = "data_tab",
+        h2("Data Insights"),
+        # Add any UI elements for data insights here
+        # For example, you can use plotly, ggplot2, or any other visualization library to show data insights.
+        # Example: plotOutput("data_plot")
+      ),
+      # PDF Files tab
+      tabItem(
+        tabName = "pdf_tab",
+        h2("PDF Files"),
+        p("Here, you can access and view the PDF files relevant to organization."),
+        # Add any UI elements related to PDF files here
+      )
+    )
+  )
+)
+
+# Server
+server <- function(input, output) {
+  # Server logic for the Data Insights tab (if needed)
+  
+  # Server logic for the PDF Files tab (if needed)
+}
+
+# Run the app
+shinyApp(ui, server)
+
